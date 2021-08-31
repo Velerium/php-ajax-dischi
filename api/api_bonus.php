56b2d6d8f28cf87ec3d34085c792c9660cce3c9a
Velerium <bondaniandrea@libero.it>
2021-08-31 14:27:56 +0200

    Bonus 1 improved: genres can now be filtered through a select menu! Thanks Ottavio :3

--- api/api_bonus.php
+++ api/api_bonus.php
@@ -2,13 +2,17 @@
 
 require_once __DIR__ . "/../database/music_db.php";
 
-$genreFilter = "Pop";  //edit this to change displayed records. Tried a select & submit, but no dice. Yet.
+$chosenGenre = $_GET['genre'];  //edit this to change displayed records. Tried a select & submit, but no dice. Yet.
 
-$filterGenre = function($item) use ($genreFilter) {
-    return strtolower($item["genre"]) === strtolower($genreFilter);
+if ($chosenGenre === "All") {
+    $result = $records;
+} else {
+    $filterGenre = function($item) use ($chosenGenre) {
+        return ($item["genre"]) === $chosenGenre;
+    };
+    $result = array_filter($records, $filterGenre);
 };
 
-$result = array_filter($records, $filterGenre);
 
 header('Content-Type: application/json');
 echo json_encode($result);
--- js/script_bonus1.js
+++ js/script_bonus1.js
@@ -5,12 +5,21 @@ new Vue (
         el: '#app',
 
         data: {
-            records: []
+            records: [],
+            selectedGenre: 'All',
+        },
+
+        methods: {
+            filter() {
+                axios.get(`http://localhost/Esercizi/30-08%20(PHP%20+%20Vue)/php-ajax-dischi/api/api_bonus.php/?genre=${this.selectedGenre}`).then((reply) => {
+                this.records = reply.data
+            })
+            }
         },
 
         created() {
 
-            axios.get('http://localhost/Esercizi/30-08%20(PHP%20+%20Vue)/php-ajax-dischi/api/api_bonus.php').then((reply) => {
+            axios.get(`http://localhost/Esercizi/30-08%20(PHP%20+%20Vue)/php-ajax-dischi/api/api_bonus.php/?genre=All`).then((reply) => {
                 this.records = reply.data
             })
         },
